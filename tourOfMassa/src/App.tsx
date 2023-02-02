c114a4dc4fe386a4d832aa36f1bb45913ab2957e
0xMazout <lf@massa.net>
2023-02-02 11:26:34 +0100

    Clean

--- tourOfMassa/src/App.tsx
+++ tourOfMassa/src/App.tsx
@@ -1,12 +1,8 @@
 import Home from './Pages/Home';
-import { Routes, Route, Outlet, Link } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      {/* <Home /> */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Home />
     </div>
   );
 }
